829d4fb654239168950982ffd981dc47a97bd9dd
Hardworking-qf <52162311+Hardworking-qf@users.noreply.github.com>
2020-07-12 22:50:55 +0800

    Update OrderUserMenuRepository.java

--- src/main/java/com/abee/cafe/dao/OrderUserMenuRepository.java
+++ src/main/java/com/abee/cafe/dao/OrderUserMenuRepository.java
@@ -1,4 +1,10 @@
 package com.abee.cafe.dao;
 
-public interface OrderUserMenuRepository {
+import com.abee.cafe.entity.OrderUserMenu;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderUserMenuRepository extends JpaRepository<OrderUserMenu, Long> {
+    OrderUserMenu findOrderUserMenuByOrderID(Long OrderID);
+
+    OrderUserMenu findOrderUserMenuByUserID(Long UserID);
 }
