92db14f69ede8b4b9e5def79faee15e1bae6ac20
Hardworking-qf <52162311+Hardworking-qf@users.noreply.github.com>
2020-07-12 21:50:48 +0800

    Update OrderUserMenu.java

--- src/main/java/com/abee/cafe/entity/OrderUserMenu.java
+++ src/main/java/com/abee/cafe/entity/OrderUserMenu.java
@@ -12,5 +12,31 @@ import javax.persistence.*;
 @Entity
 @Table(name = "order-user-menu")
 public class OrderUserMenu {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long orderID;
+
+    private Long userID;
+
+    private Long itemID;
+
+    private String username;
+
+    private String name;
+
+    private String descirbe;
+
+    private String category;
+
+    private Long amount;
+
+    private float price;
+
+    private String icon;
+
+    private Long time;
+
+    private String status;
 }
